dc457ffdf22628debd0c4001555191095556acdf
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-04-23 11:48:40 -0500

    Testability Update

---
+++ Assets/Scripts/tempGoal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempGoal : MonoBehaviour {
+
+    [SerializeField]
+    private Transform ballSpawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            other.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            other.GetComponent<Transform>().position = ballSpawnPoint.position;
+        }
+    }
+}
