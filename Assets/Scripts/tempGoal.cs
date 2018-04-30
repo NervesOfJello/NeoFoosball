e203470600ea9e75ba630d41fe607fe6c0bcf029
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-04-30 12:05:09 -0500

    Playtesting Polish

--- Assets/Scripts/tempGoal.cs
+++ Assets/Scripts/tempGoal.cs
@@ -7,11 +7,14 @@ public class tempGoal : MonoBehaviour {
     [SerializeField]
     private Transform ballSpawnPoint;
 
+    [SerializeField]
+    private int LaunchForceZ;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
-            other.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            other.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -LaunchForceZ);
             other.GetComponent<Transform>().position = ballSpawnPoint.position;
         }
     }
