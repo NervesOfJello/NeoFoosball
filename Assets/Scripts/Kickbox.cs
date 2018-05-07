d2ae971c6b66288a753812821e80b90aa211db82
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-05-07 03:57:54 -0500

    Switched input from bumpers to Triggers

--- Assets/Scripts/Kickbox.cs
+++ Assets/Scripts/Kickbox.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class Kickbox : MonoBehaviour {
 
-
+    //component variables
     private Kicker parentKicker;
     private Rigidbody ballRigidbody;
 
+    //variables for treating the triggers like buttons
+    private bool triggerDown = false;
+    private bool canKick = false;
+    private float triggerkWindowTime = 0.1f;
+
+    //input string property
     private string KickInput
     {
         get { return "Fire" + parentKicker.PlayerNumber + parentKicker.KickerNumber; }
     }
+
 	// Use this for initialization
 	void Start ()
     {
@@ -21,21 +28,49 @@ public class Kickbox : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		
+        GetTriggerDown();
 	}
 
+    //Code to treat the trigger pull like Input.GetButtonDown
+    private void GetTriggerDown()
+    {
+        if (Input.GetAxis(KickInput) != 0)
+        {
+            if (triggerDown == false)
+            {
+                StartCoroutine(TriggerDownWindowCoroutine());
+                triggerDown = true;
+            }
+            
+        }
+        else
+        {
+            triggerDown = false;
+        }
+    }
+
+    //provides a very short window after the trigger pull in which the kick will function
+    private IEnumerator TriggerDownWindowCoroutine()
+    {
+        canKick = true;
+        yield return new WaitForSeconds(triggerkWindowTime);
+        canKick = false;
+    }
+
+    //while the ball is in the trigger, and the trigger has just been pulled, kick the ball
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Ball" && Input.GetButtonDown(KickInput))
+        if (other.tag == "Ball" && canKick)
         {
             Kick(other.GetComponent<Rigidbody>());
         }
     }
 
+    //adds force to the ball with a vector with rotation equivalent to the kicker's rotation and multiplied magnitude
     private void Kick(Rigidbody rigidbody)
     {
+        canKick = false;
         Vector3 forceVector = new Vector3(transform.forward.x, 0, transform.forward.z) * parentKicker.KickForce;
-        //rigidbody.velocity = new Vector3(transform.forward.x, 0, transform.forward.z) * parentKicker.KickForce;
         rigidbody.AddForce(forceVector, ForceMode.VelocityChange);
     }
 }
