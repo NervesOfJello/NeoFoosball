fc02c43c5ab847dfa1e82e0e394cad33c3782ec2
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-05-07 10:46:00 -0500

    Build Ready

---
+++ Assets/Scripts/BallDropper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDropper : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject ball;
+    [SerializeField]
+    private float dropInterval;
+    [SerializeField]
+    private float initialWait;
+	// Use this for initialization
+	void Start () 
+	{
+        StartCoroutine(initialWaitCoroutine());
+	}
+	
+	private IEnumerator dropPeriodicallyCoroutine()
+    {
+        Destroy(Instantiate(ball,this.transform), 8);
+        yield return new WaitForSeconds(dropInterval);
+        
+        StartCoroutine(dropPeriodicallyCoroutine());
+    }
+    private IEnumerator initialWaitCoroutine()
+    {
+        yield return new WaitForSeconds(initialWait);
+        StartCoroutine(dropPeriodicallyCoroutine());
+    }
+}
